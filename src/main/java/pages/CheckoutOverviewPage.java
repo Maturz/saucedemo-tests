01f6285cd63c8831722bd81c32cfaffad7e76239
Maturz <kirya_yurevich_01@icloud.com>
2026-04-06 11:24:47 +0200

    Update CheckoutOverviewPage.java

--- src/main/java/pages/CheckoutOverviewPage.java
+++ src/main/java/pages/CheckoutOverviewPage.java
@@ -13,7 +13,7 @@ public class CheckoutPage {
     private static final By LAST_NAME = By.id("last-name");
     private static final By ZIP_CODE = By.id("postal-code");
     private static final By CONTINUE_BUTTON = By.id("continue");
-    private static final Duration TIMEOUT = Duration.ofSeconds(20);
+    private static final Duration TIMEOUT = Duration.ofSeconds(25); // ✅ для Chrome
 
     private final WebDriver driver;
     private final WebDriverWait wait;
@@ -24,7 +24,7 @@ public class CheckoutPage {
     }
 
     public void fillForm() {
-        
+        // Chrome + Firefox OK
         WebElement firstNameField = wait.until(ExpectedConditions.elementToBeClickable(FIRST_NAME));
         firstNameField.click();
         firstNameField.clear();
