274ebf645c4de3c98a82d157975a8789a1d284ca
Maturz <kirya_yurevich_01@icloud.com>
2026-04-06 12:28:15 +0200

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,19 +1,29 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
 import utils.DriverFactory;
+import pages.*;
 
 public class BaseTest {
+    protected LoginPage loginPage;
+    protected InventoryPage inventoryPage;
+    protected CartPage cart;
+    protected CheckoutPage checkoutPage;
+    protected CheckoutOverviewPage checkoutOverviewPage;
 
     @Parameters("browser")
     @BeforeMethod
-    public void setUp(@Optional("firefox") String browser) {  
+    public void setUp(@Optional("firefox") String browser) {
         DriverFactory.initDriver(browser);
         DriverFactory.getDriver().get("https://www.saucedemo.com/");
+        
+        // ✅ Инициализация page objects
+        loginPage = new LoginPage(DriverFactory.getDriver());
+        inventoryPage = new InventoryPage(DriverFactory.getDriver());
+        cart = new CartPage(DriverFactory.getDriver());
+        checkoutPage = new CheckoutPage(DriverFactory.getDriver());
+        checkoutOverviewPage = new CheckoutOverviewPage(DriverFactory.getDriver());
     }
 
     @AfterMethod
