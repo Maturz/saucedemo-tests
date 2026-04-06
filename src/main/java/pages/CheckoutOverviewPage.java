bec6cef26a6d219af78496b67e7b3756b4039b5c
Maturz <kirya_yurevich_01@icloud.com>
2026-04-06 10:53:14 +0200

    Update CheckoutOverviewPage.java

--- src/main/java/pages/CheckoutOverviewPage.java
+++ src/main/java/pages/CheckoutOverviewPage.java
@@ -8,39 +8,37 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class CheckoutOverviewPage {
+    private static final By FINISH_BUTTON = By.id("finish");
+    private static final By ITEM_TOTAL_LABEL = By.className("summary_subtotal_label");
+    private static final By SUCCESS_HEADER = By.className("complete-header");
+    private static final String ITEM_TOTAL_PREFIX = "Item total: $";
+    private static final Duration TIMEOUT = Duration.ofSeconds(15); // для Chrome
 
-    WebDriver driver;
-    WebDriverWait wait;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
 
     public CheckoutOverviewPage(WebDriver driver) {
         this.driver = driver;
-//        TODO please extract timeout value to a constant and use it here
-        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.wait = new WebDriverWait(driver, TIMEOUT);
     }
 
     public double getItemTotalValue() {
-
         String totalText = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.className("summary_subtotal_label"))
+                ExpectedConditions.visibilityOfElementLocated(ITEM_TOTAL_LABEL)
         ).getText();
-        //TODO - please dont use magic strings, extract "Item total: $" to a constant and use it here
-        String value = totalText.replace("Item total: $", "");
+        String value = totalText.replace(ITEM_TOTAL_PREFIX, "");
         return Double.parseDouble(value);
     }
 
-    public void finish() {
-        //TODO please extract selectors to constants and use them here
-        //TODO - I recommend you to find more suitable name for this method,
-        // because it is not clear what exactly it does,
-        // and it is not clear that it returns item total value
-//        TODO Why we use both wait for visibility and clickability? I think it is enough to wait for clickability, because it already implies visibility
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("finish")));
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("finish"))).click();
+    
+    public void completeCheckout() {
+        wait.until(ExpectedConditions.elementToBeClickable(FINISH_BUTTON)).click();
     }
 
     public String getSuccessMessage() {
         return wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.className("complete-header"))
+                ExpectedConditions.visibilityOfElementLocated(SUCCESS_HEADER)
         ).getText();
     }
+}
 }
