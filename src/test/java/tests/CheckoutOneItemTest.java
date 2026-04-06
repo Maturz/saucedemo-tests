6ceeca907d70b6a594184b95ee220569b1a1c9cf
Maturz <kirya_yurevich_01@icloud.com>
2026-04-06 21:50:24 +0200

    Update CheckoutOneItemTest.java

--- src/test/java/tests/CheckoutOneItemTest.java
+++ src/test/java/tests/CheckoutOneItemTest.java
@@ -8,16 +8,13 @@ import pages.*;
 public class CheckoutOneItemTest extends BaseTest {
 
     @Test
-    // TODO this test is never throwing InterruptedException, so we can safely remove it from method signature
-    public void testOneItemCheckout() {  // ✅ Copilot #1 & #2 FIXED!
+    public void testOneItemCheckout() { 
         
-        // ✅ extends BaseTest = DriverFactory автоматически
         loginPage.login("standard_user", "secret_sauce");
 
         inventoryPage.addProduct("Sauce Labs Backpack");
         inventoryPage.openCart();
 
-        // ✅ DRY принцип
         Assert.assertTrue(cart.isProductInCart("Sauce Labs Backpack"));
 
         cart.clickCheckout();
